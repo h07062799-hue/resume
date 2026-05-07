dffc6fdea7b6f46e71e460fb7a2e91f174bba887
h07062799-hue <h07062799@gmail.com>
2026-05-07 14:48:26 +0530

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Resume from './resume';
+
+function App() {
+    return (
+        <div>
+            <Resume />
+        </div>
+    );
+}
+
+export default App;
